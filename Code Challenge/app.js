12d6a7bc3d20186e36355abb09ba5ac7880233b8
Otoniel Carlos <otonielcarlosf@gmail.com>
2016-03-24 09:34:32 -0600

    new update

--- Code Challenge/app.js
+++ Code Challenge/app.js
@@ -1,42 +1,45 @@
-function newToDo() {
-    var todos = [];
-    var newTodo = sessionStorage.getItem('todo');
-    if (newTodo !== null) {
-        todos = JSON.parse(newTodo);
+var GetItem = function () {
+    this.item = sessionStorage.getItem('todo');
+    this.todo = "";
+    if (this.item !== null) {
+        this.todo = JSON.parse(this.item);
     }
-    return todos;
+    return this.todo;
 }
-
-function displayList() {
-    var todos = newToDo();
-    var list = '<ul>';
-    for (var i = 0; i < todos.length; i++) {
-        list += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
-    };
-    list += '</ul>';
-    document.getElementById('todos').innerHTML = list;
-    var buttons = document.getElementsByClassName('remove');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
+var ToDo = function () {
+    this.item = GetItem();
 }
 
 function addTodo() {
-    var task = document.getElementById('task').value;
-    var todos = newToDo();
-    todos.push(task);
-    sessionStorage.setItem('todo', JSON.stringify(todos));
+    var todo = new ToDo();
+    todo.item = document.getElementById('task').value;
+    todoArray.push(todo.item);
+    sessionStorage.setItem('todo', JSON.stringify(todoArray));
     displayList();
     return false;
 }
 
-function remove() {
-    var id = this.getAttribute('id');
-    var todos = newToDo();
-    todos.splice(id, 1);
-    sessionStorage.setItem('todo', JSON.stringify(todos));
-    displayList();
-    return false;
+//function remove() {
+//    var todo = new ToDo();
+//    var id = todo.id;
+//    todo.todos.splice(id, 1);
+//    sessionStorage.setItem('todo', JSON.stringify(todo.todos));
+//    displayList();
+//    return false;
+//}
+var todoArray = [];
+function displayList() {
+    var todo = new ToDo();
+    var list = '<ul>';
+    for (var i = 0; i < todoArray.length; i++) {
+        list += '<li>' + todoArray[i] + '<button class="remove" id="' + i + '">x</button></li>';
+    };
+    list += '</ul>';
+    document.getElementById('todos').innerHTML = list;
+//    var buttons = document.getElementsByClassName('remove');
+//    for (var i = 0; i < buttons.length; i++) {
+//        buttons[i].addEventListener('click', remove);
+//    };
 }
 
 document.getElementById('add').addEventListener('click', addTodo);
