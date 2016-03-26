5159867446e34a4096721c873ba9eeebd94ad0b4
Otoniel Carlos <otonielcarlosf@gmail.com>
2016-03-26 12:05:18 -0600

    new update

--- Code Challenge/app.js
+++ Code Challenge/app.js
@@ -1,36 +1,35 @@
- var ToDo = function () {
-     this.todosArray = [];
-    ToDo.prototype.items = function () {
-        this.item = localStorage.getItem('todo');
+ ToDo = function () {
+    this.items = function () {
+        this.item = sessionStorage.getItem('todo');
         this.todo = "";
         if (this.item !== null) {
             this.todo = JSON.parse(this.item);
         }
         return this.todo;
     };
-    ToDo.prototype.addTodo = function () {
+    this.addTodo = function () {
         this.items = document.getElementById('task').value;
-        this.todosArray.push(this.items);
-        localStorage.setItem('todo', JSON.stringify(this.todosArray));
+        todosArray.push(this.items);
+        sessionStorage.setItem('todo', JSON.stringify(todosArray));
         document.getElementById('task').value = "";
         displayList();
         return false;
     };
-    ToDo.prototype.remove = function () {
+    this.remove = function () {
         this.id = this.getAttribute('id');
-        this.todosArray.splice(this.id, 1);
-        localStorage.setItem('todo', JSON.stringify(this.todosArray));
+        todosArray.splice(this.id, 1);
+        sessionStorage.setItem('todo', JSON.stringify(todosArray));
         displayList();
         return false;
     };
 };
 var todo = new ToDo();
-//var todosArray = [];
+var todosArray = [];
 
 function displayList() {
     var list = '<ul>';
-    for (var i = 0; i < todo.todosArray.length; i++) {
-        list += '<li>' + '<button class="remove" id=" "'+ i +'" ">X</button>'+' ' +todo.todosArray[i] + '</li>';
+    for (var i = 0; i < todosArray.length; i++) {
+        list += '<li>' + '<button class="remove" id=" "'+ i +'" ">X</button>'+' ' +todosArray[i] + '</li>';
     };
     list += '</ul>';
     document.getElementById('todos').innerHTML = list;
