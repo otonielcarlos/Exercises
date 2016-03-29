41d81bb7fee4296a63a423dabba667ede0e72a36
Otoniel Carlos <otonielcarlosf@gmail.com>
2016-03-28 23:48:25 -0600

    new update

--- Code Challenge/app.js
+++ Code Challenge/app.js
@@ -1,49 +1,68 @@
- ToDo = function () {
-    this.items = function () {
-        this.item = sessionStorage.getItem('todo');
-        this.todo = "";
-        if (this.item !== null) {
-            this.todo = JSON.parse(this.item);
-        }
-        return this.todo;
-    };
-    this.addTodo = function () {
-        this.items = document.getElementById('task').value;
-        todosArray.push(this.items);
-        sessionStorage.setItem('todo', JSON.stringify(todosArray));
-        document.getElementById('task').value = "";
-        displayList();
-        return false;
-    };
-    this.remove = function () {
-        this.id = this.getAttribute('id');
-        todosArray.splice(this.id, 1);
-        sessionStorage.setItem('todo', JSON.stringify(todosArray));
-        displayList();
-        return false;
-    };
+var ToDo = function () {
+    this.todosArray = [];
 };
+
+ToDo.prototype.items = function () {
+    this.item = sessionStorage.getItem('todo');
+    this.todo = "";
+    if (this.item !== null) {
+        this.todo = JSON.parse(this.item);
+    }
+    return this.todo;
+};
+
+ToDo.prototype.addTodo = function (item) {
+    this.todosArray.push(item);
+    sessionStorage.setItem('todo', JSON.stringify(this.todosArray));
+    displayList();
+    return false;
+};
+
+ToDo.prototype.remove = function (position) {
+    this.todosArray.splice(position, 1);
+    sessionStorage.setItem('todo', JSON.stringify(this.todosArray));
+    displayList();
+    return false;
+};
+
 var todo = new ToDo();
-var todosArray = [];
 
 function displayList() {
     var list = '<ul>';
-    for (var i = 0; i < todosArray.length; i++) {
-        list += '<li>' + '<button class="remove" id=" "'+ i +'" ">X</button>'+' ' +todosArray[i] + '</li>';
+    for (var i = 0; i < todo.todosArray.length; i++) {
+        var item = todo.todosArray[i];
+        list += '<li>' + '<button class="remove" id=" ' + i + '">X</button>' + ' ' + item + '</li>';
     };
     list += '</ul>';
     document.getElementById('todos').innerHTML = list;
     var buttons = document.getElementsByClassName('remove');
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', todo.remove);
+        buttons[i].addEventListener('click', function () {
+            var id = this.getAttribute('id');
+//            console.log(id);
+            todo.remove(id);
+            console.log(id);
+//            return false;
+        });
     };
 };
-document.getElementById('add').addEventListener('click', todo.addTodo);
+
+document.getElementById('add').addEventListener('click', function () {
+    var item = document.getElementById('task').value;
+    todo.addTodo(item);
+    console.log(item);
+    document.getElementById('task').value = "";
+    return false;
+});
+
 document.getElementById('task').onkeypress = function (e) {
+    var item = document.getElementById('task').value;
     if (!e) e = window.event;
     if (e.keyCode == '13') {
-        todo.addTodo();
+        todo.addTodo(item);
+        document.getElementById('task').value = "";
         return false;
     };
 };
+
 displayList();
